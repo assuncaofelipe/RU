fa424dbb20845513ef30d527987eca0ace3913d3
Felipe V Assunção <felippevassuncao@gmail.com>
2022-08-15 13:45:20 -0400

    criacao dos cards na tela de fichas

--- app/src/main/java/com/assuncao/ru/FichasRefeicaoActivity.kt
+++ app/src/main/java/com/assuncao/ru/FichasRefeicaoActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import com.assuncao.ru.databinding.ActivityFichasRefeicaoBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
-import io.grpc.okhttp.internal.Util
 
 class FichasRefeicaoActivity : AppCompatActivity() {
 
@@ -25,16 +24,15 @@ class FichasRefeicaoActivity : AppCompatActivity() {
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.arrow_back)
 
 
-
         // Firebase
-        user = FirebaseAuth.getInstance()
+
+        /*user = FirebaseAuth.getInstance()
         if (user.currentUser != null) {
             user.currentUser?.let {
                 binding.tvResultUser.text = it.email
             }
-        }
+        } */
         db = FirebaseFirestore.getInstance()
-
         // chama o ouvinte de aluno
         ouve_aluno()
 
@@ -43,27 +41,30 @@ class FichasRefeicaoActivity : AppCompatActivity() {
     // chamase-se "ouve_aluno()" pois o metodo addOnCompleteListener
     // é um ouvinte do documento que pertence a colecao
     private fun ouve_aluno() {
-
         db.collection("Alunos")
             .document("9H4yPmDaTgNufH5HMIE6WJ74Ip83").get()
             .addOnSuccessListener { documento ->
 
-                if ( documento != null && documento.exists()) {
+                if (documento != null && documento.exists()) {
                     val dados = documento.data
                     val desjejum = dados?.get("desjejum")
                     val almoco = dados?.get("almoco")
                     val jantar = dados?.get("jantar")
 
-                    var txt_desjejum = findViewById<TextView>(R.id.tv_qtd_desjejum)
-                    var txt_almoco = findViewById<TextView>(R.id.tv_qtd_almoco)
-                    var txt_jantar = findViewById<TextView>(R.id.tv_qtd_jantar)
+                    val txt_desjejum = findViewById<TextView>(R.id.qtd_ficha_desjejum)
+                    val txt_almoco = findViewById<TextView>(R.id.qtd_ficha_almoco)
+                    val txt_jantar = findViewById<TextView>(R.id.qtd_ficha_jantar)
 
-                    txt_desjejum.setText("$desjejum")
-                    txt_almoco.setText("$almoco")
-                    txt_jantar.setText("$jantar")
+                    txt_desjejum.text = "$desjejum"
+                    txt_almoco.text = "$almoco"
+                    txt_jantar.text = "$jantar"
 
                 } else {
-                    Toast.makeText(baseContext, "error ao ler documento, ele não existe ou está vazio", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        baseContext,
+                        "error ao ler documento, ele não existe ou está vazio",
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
 
             }.addOnFailureListener { error ->
