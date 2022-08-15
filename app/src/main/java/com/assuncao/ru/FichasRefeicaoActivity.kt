ab0afb651d9c3b3bf3b9139401125952e32e515f
Felipe V Assunção <felippevassuncao@gmail.com>
2022-08-15 15:54:34 -0400

    ajustes

--- app/src/main/java/com/assuncao/ru/FichasRefeicaoActivity.kt
+++ app/src/main/java/com/assuncao/ru/FichasRefeicaoActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.assuncao.ru.databinding.ActivityFichasRefeicaoBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
@@ -14,37 +15,31 @@ class FichasRefeicaoActivity : AppCompatActivity() {
     private lateinit var user: FirebaseAuth
     private lateinit var db: FirebaseFirestore
 
+    // refresh layout
+    private lateinit var swipeRefresh: SwipeRefreshLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityFichasRefeicaoBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_fichas_refeicao)
 
         // set Arrow Back to Menu
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         supportActionBar!!.setHomeAsUpIndicator(R.drawable.arrow_back)
 
-
-        // Firebase
-
-        /*user = FirebaseAuth.getInstance()
-        if (user.currentUser != null) {
-            user.currentUser?.let {
-                binding.tvResultUser.text = it.email
-            }
-        } */
+        // Aciona a instancia do Firebase pelo db
         db = FirebaseFirestore.getInstance()
-        // chama o ouvinte de aluno
-        ouve_aluno()
+
+        // chama a função ouvinte de aluno
+        headerStudent()
 
     }
 
     // chamase-se "ouve_aluno()" pois o metodo addOnCompleteListener
     // é um ouvinte do documento que pertence a colecao
-    private fun ouve_aluno() {
+    private fun headerStudent() {
         db.collection("Alunos")
             .document("9H4yPmDaTgNufH5HMIE6WJ74Ip83").get()
             .addOnSuccessListener { documento ->
-
                 if (documento != null && documento.exists()) {
                     val dados = documento.data
                     val desjejum = dados?.get("desjejum")
@@ -70,8 +65,6 @@ class FichasRefeicaoActivity : AppCompatActivity() {
             }.addOnFailureListener { error ->
                 Toast.makeText(baseContext, "Error", Toast.LENGTH_SHORT).show()
             }
-
-
     }
 }
 
