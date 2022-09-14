c8208724c9113d47f05d8c8b3f085051d62588dc
Felipe V Assunção <felippevassuncao@gmail.com>
2022-09-14 01:18:15 -0400

    criação de activitys e componentes, além de ajustes de interface

--- app/src/main/java/com/assuncao/ru/ui/SobreActivity.kt
+++ app/src/main/java/com/assuncao/ru/ui/SobreActivity.kt
@@ -25,9 +25,9 @@ class SobreActivity : AppCompatActivity() {
         val imageList = ArrayList<SlideModel>()
 
         imageList.add(SlideModel(R.drawable.icet))
-        imageList.add(SlideModel(R.drawable.icet))
-        imageList.add(SlideModel(R.drawable.icet))
-        imageList.add(SlideModel(R.drawable.icet))
+        imageList.add(SlideModel(R.drawable.icet_um))
+        imageList.add(SlideModel(R.drawable.icet_dois))
+        imageList.add(SlideModel(R.drawable.icet_tres))
 
         imageSlider.setImageList(imageList, ScaleTypes.CENTER_CROP)
     }
@@ -36,10 +36,12 @@ class SobreActivity : AppCompatActivity() {
         val imageSlider = findViewById<ImageSlider>(R.id.imageSliderAutores)
         val imageList = ArrayList<SlideModel>()
 
-        imageList.add(SlideModel(R.drawable.avatar))
-        imageList.add(SlideModel(R.drawable.avatar))
-        imageList.add(SlideModel(R.drawable.avatar))
-        imageList.add(SlideModel(R.drawable.avatar))
+        imageList.add(SlideModel(R.drawable.vandermir, "  Professor Dr. Vandermir Silva"))
+        imageList.add(SlideModel(R.drawable.assuncao, "  Felipe Assunção | Developer"))
+        imageList.add(SlideModel(R.drawable.filipe, "  Filipe Dias | Scrum Master"))
+        imageList.add(SlideModel(R.drawable.thiago, "  Thiago Rodrigues | Documentation"))
+        imageList.add(SlideModel(R.drawable.joao, "  João Victor | Designer and Documentation"))
+        imageList.add(SlideModel(R.drawable.daniel, "  Carlos Daniel | Suporte"))
 
         imageSlider.setImageList(imageList, ScaleTypes.CENTER_CROP)
     }
